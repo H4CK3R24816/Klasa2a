6d07715f28a2a6b9703b4786135c417337820b51
Buddy24816 <jnosila@wp.pl>
2025-03-13 16:06:38 +0100

    Add files via upload

---
+++ C++/Vector.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+bool liczba = true;
+vector<int> proba;
+int a, b;
+int main() {
+	cout << "Ile liczb: ";
+	cin >> b;
+
+
+	for (int o = 0; o < b; o++) {		//dopisuje do vectoru tyle liczb podanych ile na początku chiał użytkownik( cin>>b)
+		cin >> a;
+		proba.push_back(a);
+	}
+
+
+	for (int i = 0; i < proba.size(); i++) {		//Wypisuje vector po kolei
+		cout << endl << proba[i];
+	}
+	cout << endl;
+
+
+
+	return 0;
+}
